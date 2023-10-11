1d92500b8e5b1fb6cb95cfca2777aa86278e2d61
pawan-live <pawan.senpura@gmail.com>
2023-10-12 02:06:02 +0530

    add dotenv and update gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,17 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Supplier } from './typeorm/entities/Supplier';
 import { SuppliersModule } from './suppliers/suppliers.module';
+import 'dotenv/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'db-procx-do-user-13171573-0.b.db.ondigitalocean.com',
-      port: 25060,
-      username: 'doadmin',
-      password: 'AVNS_RTTlLG6iD1sutqHe2K6',
-      database: 'defaultdb',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DEFAULT_SCHEMA,
       entities: [Supplier],
       synchronize: true, // every single time we do a modification, then this automatically updates
     }),
