0e98df51cb77a4b8f409cbfdbd9717ca17c9456c
Nisal <it21259302@my.sliit.lk>
2023-10-12 17:28:23 +0530

    Items CRUD

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ItemsController } from './controllers/items/items.controller';
 import { ItemsService } from './services/items/items.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from 'src/typeorm/entities/Item';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Item])],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
