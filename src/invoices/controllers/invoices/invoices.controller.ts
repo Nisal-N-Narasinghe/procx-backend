922ed767058ed9ef809b9f1bd7375abddd370082
Nisal <it21259302@my.sliit.lk>
2023-10-14 03:13:35 +0530

    invoices API

--- src/invoices/controllers/invoices/invoices.controller.ts
+++ src/invoices/controllers/invoices/invoices.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateInvoiceDto } from 'src/invoices/dtos/CreateInvoice.dto';
+import { UpdateInvoiceDto } from 'src/invoices/dtos/UpdateInvoice.dto';
+import { InvoicesService } from 'src/invoices/services/invoices/invoices.service';
+import { Invoice } from 'src/typeorm/entities/Invoice';
 
 @Controller('invoices')
-export class InvoicesController {}
+export class InvoicesController {
+  constructor(private invoiceService: InvoicesService) {}
+
+  @Get()
+  async getInvoice() {
+    return this.invoiceService.findInvoice();
+  }
+
+  @Post()
+  createInvoice(@Body() CreateInvoiceDto: CreateInvoiceDto) {
+    return this.invoiceService.createInvoice(CreateInvoiceDto);
+  }
+
+  @Put(':id')
+  async updateInvoice(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() UpdateInvoiceDto: UpdateInvoiceDto,
+  ): Promise<Invoice> {
+    return this.invoiceService.updateInvoice(id, UpdateInvoiceDto);
+  }
+
+  @Delete(':id')
+  async deleteInvoice(@Param('id', ParseIntPipe) id: number): Promise<Invoice> {
+    return await this.invoiceService.deleteInvoice(id);
+  }
+}
--- src/invoices/services/invoices/invoices.service.ts
+++ src/invoices/services/invoices/invoices.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Invoice } from 'src/typeorm/entities/Invoice';
+import {
+  CreateInvoiceParams,
+  UpdateInvoiceParams,
+} from 'src/utils/InvoiceTypes';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class InvoicesService {}
+export class InvoicesService {
+  constructor(
+    @InjectRepository(Invoice)
+    private invoiceRepository: Repository<Invoice>,
+  ) {}
+
+  findInvoice() {
+    return this.invoiceRepository.find();
+  }
+
+  createInvoice(invoiceDetails: CreateInvoiceParams) {
+    const newInvoice = this.invoiceRepository.create({
+      ...invoiceDetails,
+      createdAt: new Date(),
+    });
+    return this.invoiceRepository.save(newInvoice); // async, so return the promise and wait
+  }
+
+  async updateInvoice(
+    id,
+    updateInvoiceDetails: UpdateInvoiceParams,
+  ): Promise<Invoice> {
+    const existingInvoice = await this.invoiceRepository.findOneBy({ id });
+    if (!existingInvoice) {
+      throw new NotFoundException(`Invoice with ID ${id} not found`);
+    }
+    const updatedInvoice = { ...existingInvoice, ...updateInvoiceDetails };
+    await this.invoiceRepository.save(updatedInvoice);
+    return updatedInvoice;
+  }
+
+  async deleteInvoice(id): Promise<Invoice> {
+    const deletedInvoice = await this.invoiceRepository.findOneBy({ id });
+    if (!deletedInvoice) {
+      throw new NotFoundException('Invoice not found');
+    }
+    await this.invoiceRepository.delete({ id });
+    return deletedInvoice;
+  }
+}
