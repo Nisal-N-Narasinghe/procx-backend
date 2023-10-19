c734649aa963ee9c0a408ddda023e74cb2987b97
Nisal <it21259302@my.sliit.lk>
2023-10-19 13:20:02 +0530

    add supplier for an order - Order side end point

--- src/orders/services/orders/orders.service.ts
+++ src/orders/services/orders/orders.service.ts
@@ -7,10 +7,10 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Item } from 'src/typeorm/entities/Item';
 import { Order } from 'src/typeorm/entities/Order';
-// import { Supplier } from 'src/typeorm/entities/Supplier';
+import { Supplier } from 'src/typeorm/entities/Supplier';
 import { CreateItemParams } from 'src/utils/ItemTypes';
 import { CreateOrderParams, UpdateOrderParams } from 'src/utils/OrderTypes';
-// import { CreateSupplierParams } from 'src/utils/SupplierTypes';
+import { CreateSupplierParams } from 'src/utils/SupplierTypes';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -18,8 +18,8 @@ export class OrdersService {
   constructor(
     @InjectRepository(Order)
     private orderRepository: Repository<Order>,
-    // @InjectRepository(Supplier)
-    // private supplierRepository: Repository<Supplier>,
+    @InjectRepository(Supplier)
+    private supplierRepository: Repository<Supplier>,
     @InjectRepository(Item)
     private itemRepository: Repository<Item>,
   ) {}
@@ -36,22 +36,22 @@ export class OrdersService {
     return this.orderRepository.save(newOrder); // async, so return the promise and wait
   }
 
-  // async createSupplier(id: number, supplierDetails: CreateSupplierParams) {
-  //   const order = await this.orderRepository.findOneBy({ id });
-  //   if (!order) {
-  //     throw new HttpException(
-  //       `Order with ID ${id} not found`,
-  //       HttpStatus.BAD_REQUEST,
-  //     );
-  //   }
-  //   const newSupplier = this.supplierRepository.create({
-  //     ...supplierDetails,
-  //     createdAt: new Date(),
-  //   });
-  //   const savedSupplier = await this.supplierRepository.save(newSupplier);
-  //   order.supplier = savedSupplier;
-  //   return this.orderRepository.save(order); // async, so return the promise and wait
-  // }
+  async createSupplier(id: number, supplierDetails: CreateSupplierParams) {
+    const order = await this.orderRepository.findOneBy({ id });
+    if (!order) {
+      throw new HttpException(
+        `Order with ID ${id} not found`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const newSupplier = this.supplierRepository.create({
+      ...supplierDetails,
+      createdAt: new Date(),
+    });
+    const savedSupplier = await this.supplierRepository.save(newSupplier);
+    order.supplier = savedSupplier;
+    return this.orderRepository.save(order); // async, so return the promise and wait
+  }
 
   async createOrderItems(id: number, itemsDetails: CreateItemParams) {
     const order = await this.orderRepository.findOneBy({ id });
